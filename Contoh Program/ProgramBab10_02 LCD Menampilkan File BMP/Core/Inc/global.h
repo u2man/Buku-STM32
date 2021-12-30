69e1aae6b652c70338ce1808efcb301d6d402bef
Kang Usman <u_2man@yahoo.co.id>
2021-12-30 11:18:06 +0700

    Add files via upload

---
+++ Contoh Program/ProgramBab10_02 LCD Menampilkan File BMP/Core/Inc/global.h
@@ -0,0 +1,23 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 5, 2019
+ *      Author: Kang Usman
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+#include "fsmc.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "ssd1289.h"
+#include "lcd.h"
+#include "fonts.h"
+
+#endif /* INC_GLOBAL_H_ */
