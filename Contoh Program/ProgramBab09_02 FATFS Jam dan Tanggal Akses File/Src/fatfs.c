c36556f86a73c5dd0428d32a7ed2e280f947b5ea
Kang Usman <u_2man@yahoo.co.id>
2021-12-30 11:04:17 +0700

    Add files via upload

---
+++ Contoh Program/ProgramBab09_02 FATFS Jam dan Tanggal Akses File/Src/fatfs.c
@@ -0,0 +1,69 @@
+/**
+  ******************************************************************************
+  * @file   fatfs.c
+  * @brief  Code for fatfs applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+#include "fatfs.h"
+
+uint8_t retSD;    /* Return value for SD */
+char SDPath[4];   /* SD logical drive path */
+FATFS SDFatFS;    /* File system object for SD logical drive */
+FIL SDFile;       /* File object for SD */
+
+/* USER CODE BEGIN Variables */
+extern RTC_HandleTypeDef hrtc;
+/* USER CODE END Variables */
+
+void MX_FATFS_Init(void)
+{
+  /*## FatFS: Link the SD driver ###########################*/
+  retSD = FATFS_LinkDriver(&SD_Driver, SDPath);
+
+  /* USER CODE BEGIN Init */
+  /* additional user code for init */
+
+  /* USER CODE END Init */
+}
+
+/**
+  * @brief  Gets Time from RTC
+  * @param  None
+  * @retval Time in DWORD
+  */
+DWORD get_fattime(void)
+{
+  /* USER CODE BEGIN get_fattime */
+	RTC_TimeTypeDef RTCTime;
+	RTC_DateTypeDef RTCDate;
+
+	HAL_RTC_GetTime(&hrtc, &RTCTime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(&hrtc, &RTCDate, RTC_FORMAT_BIN);
+
+	  return   ((DWORD)((2000+RTCDate.Year) - 1980) << 25) /* Fixed to Jan. 1, 2011 */
+	   | ((DWORD)RTCDate.Month << 21)
+	   | ((DWORD)RTCDate.Date << 16)
+	   | ((DWORD)RTCTime.Hours << 11)
+	   | ((DWORD)RTCTime.Minutes << 5)
+	   | ((DWORD)RTCTime.Seconds >> 1);
+
+  /* USER CODE END get_fattime */
+}
+
+/* USER CODE BEGIN Application */
+     
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
